d5ea810d653a76b491971503eb0d854c8422c964
Chris P <chrisp1877@gmail.com>
2020-09-22 21:27:48 -0700

    adding more deps

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,4 +1,15 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+    dev::Server,
+};
+
+pub fn run_dev() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+    Ok(server)
+}
+
 
 #[cfg(test)]
 mod tests {
