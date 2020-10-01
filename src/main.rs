70600b508e529404e0abf2132db366c492031715
Chris P <chrisp1877@gmail.com>
2020-10-01 02:11:20 -0700

    updatin routes, static

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use divapi::{app, context};
+use std::net::TcpListener;
+
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7777").expect("Failed to bind");
+    app::run_api(listener).await?;
+    Ok(())
 }
