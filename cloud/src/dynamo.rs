4898a09ef0066c038bb88582a373d6463b6fd7e6
Chris P <chrisp1877@gmail.com>
2020-10-16 20:04:12 -0700

    adding complicated create table method to dynamo file

--- cloud/src/dynamo.rs
+++ cloud/src/dynamo.rs
@@ -4,7 +4,7 @@ use std::collections::hash_map::{HashMap, RandomState};
 use super::auth::types::CgUser;
 use rusoto_core::Region;
 use dynomite::{
-    retry::{Retries, Policy}, dynamodb::{DynamoDbClient, DynamoDb,
+    retry::{Retries, Policy}, dynamodb::{DynamoDbClient, DynamoDb, KeySchemaElement,
         PutRequest, DeleteItemInput, QueryInput, ExpectedAttributeValue, 
         AttributeDefinition,  GetItemInput, WriteRequest, PutItemInput, DeleteRequest, 
         UpdateItemInput, ListTablesInput, CreateTableInput, AttributeValue, 
@@ -44,6 +44,38 @@ impl DynamoClient {
         }).await {
            Ok(res) => Ok(()),
            Err(e) => Err(e.to_string())
+        }
+    }
+
+    pub async fn create_table(&self, 
+        table: &str, 
+        attrs_types: Vec<(&str, &str, bool)>, // name, type, is_key
+    ) -> Result<(), String> 
+    {
+        let (attrs, keys): (Vec<AttributeDefinition>, Vec<KeySchemaElement>) 
+            = attrs_types.into_iter().fold(
+                (Vec::<AttributeDefinition>::new(), Vec::<KeySchemaElement>::new()), 
+                |(mut attrs, mut keys), att| 
+            {
+                attrs.push(AttributeDefinition {
+                    attribute_name: att.0.into(),
+                    attribute_type: att.1.into(), ..Default::default()
+                });
+                if att.2 {
+                    keys.push(KeySchemaElement { //TODO deal with range key type
+                        attribute_name: att.0.into(),
+                        key_type: "HASH".into(), ..Default::default()
+                    });
+                }
+                (attrs, keys)
+            });
+        match self.db.create_table(CreateTableInput {
+            table_name: table.to_string(),
+            attribute_definitions: attrs,
+            key_schema: keys, ..Default::default()
+        }).await {
+            Ok(_resp) => Ok(()),
+            Err(err) => Err(err.to_string()),
         }
     }
 
