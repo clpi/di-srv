a54f340824bcbdc12263714cbb15918ea4385088
clpi <clp@clp.is>
2021-01-25 04:57:51 -0500

    fixing dash route

--- src/handlers/public.rs
+++ src/handlers/public.rs
@@ -18,6 +18,8 @@ pub fn routes(cfg: &mut ServiceConfig) {
         .service(user)
         .service(cover)
         .service(contact)
+        .service(dash)
+        .service(about)
         .service(self::auth::routes(""))
         .service(self::dashboard::routes("/dashboard"))
         .service(self::admin::routes("/admin"));
@@ -130,4 +132,19 @@ pub async fn user(
             .map_err(|_| actix_web::error::ErrorInternalServerError("Template error"))?;
         Ok(HttpResponse::Ok().content_type("text/html").body(s))
     }
+}
+
+#[get("/dashboard")]
+pub async fn dash(
+    _id: actix_session::Session,
+    _req: actix_web::HttpRequest,
+    _query: web::Query<HashMap<String, String>>,
+    data: web::Data<State>,) -> HttpResponse
+{
+    let _db = data.db.lock().unwrap();
+    let mut ctx = tera::Context::new();
+    let s = data.tera.render("dashboard.html", &ctx)
+            .map_err(|_| actix_web::error::ErrorInternalServerError("Template error"))
+            .unwrap_or_default();
+    HttpResponse::Ok().content_type("text/html").body(s)
 }
